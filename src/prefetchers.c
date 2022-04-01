9c03056bad7efa3760c6cdcb5d8dc72ee815c2de
Uroy01 <89428310+Uroy01@users.noreply.github.com>
2022-04-01 12:05:34 -0600

    Update prefetchers.c

--- src/prefetchers.c
+++ src/prefetchers.c
@@ -3,9 +3,9 @@
 // cache systems and defines the prefetcher struct.
 //
 
-#include "prefetchers.h"
+#include "prefetcher.h"
 
-// Null Prefetcher
+// Nul Prefetcher
 // ============================================================================
 uint32_t null_handle_mem_access(struct prefetcher *prefetcher, struct cache_system *cache_system,
                                 uint32_t address, bool is_miss)
@@ -25,20 +25,30 @@ struct prefetcher *null_prefetcher_new()
 
 // Sequential Prefetcher
 // ============================================================================
-// TODO feel free to create additional structs/enums as necessary
+// feel free to create additional structs/enums as necessary
+struct sequential_data{
+    int n;
+};
 
 uint32_t sequential_handle_mem_access(struct prefetcher *prefetcher,
                                       struct cache_system *cache_system, uint32_t address,
                                       bool is_miss)
 {
-    // TODO: Return the number of lines that were prefetched.
-    return 0;
+    if(((struct sequential_data*) prefetcher->data)->n > 0){
+        for(int i = 1; i <= ((struct sequential_data*) prefetcher->data)->n; i++){
+        uint32_t fetch_address = address + i*cache_system->line_size;
+            cache_system_mem_access(cache_system, fetch_address, 'R', true);
+        }
+    }
+    // Return the number of lines that were prefetched.
+    return ((struct sequential_data*) prefetcher->data)->n;
 }
 
 void sequential_cleanup(struct prefetcher *prefetcher)
 {
-    // TODO cleanup any additional memory that you allocated in the
+    // cleanup any additional memory that you allocated in the
     // sequential_prefetcher_new function.
+    free((struct sequential_data*) prefetcher->data);
 }
 
 struct prefetcher *sequential_prefetcher_new(uint32_t prefetch_amount)
@@ -47,8 +57,11 @@ struct prefetcher *sequential_prefetcher_new(uint32_t prefetch_amount)
     sequential_prefetcher->handle_mem_access = &sequential_handle_mem_access;
     sequential_prefetcher->cleanup = &sequential_cleanup;
 
-    // TODO allocate any additional memory needed to store metadata here and
+    // allocate any additional memory needed to store metadata here and
     // assign to sequential_prefetcher->data.
+    struct sequential_data *new_data = malloc(sizeof(struct sequential_data));
+    new_data->n = prefetch_amount;
+    sequential_prefetcher->data = new_data;
 
     return sequential_prefetcher;
 }
@@ -59,15 +72,17 @@ uint32_t adjacent_handle_mem_access(struct prefetcher *prefetcher,
                                     struct cache_system *cache_system, uint32_t address,
                                     bool is_miss)
 {
-    // TODO perform the necessary prefetches for the adjacent strategy.
-
-    // TODO: Return the number of lines that were prefetched.
-    return 0;
+    // perform the necessary prefetches for the adjacent strategy.
+    uint32_t fetch_address = address + cache_system->line_size;
+    cache_system_mem_access(cache_system, fetch_address, 'R', true);
+   
+    // Return the number of lines that were prefetched.
+    return 1;
 }
 
 void adjacent_cleanup(struct prefetcher *prefetcher)
 {
-    // TODO cleanup any additional memory that you allocated in the
+    // cleanup any additional memory that you allocated in the
     // adjacent_prefetcher_new function.
 }
 
@@ -77,7 +92,7 @@ struct prefetcher *adjacent_prefetcher_new()
     adjacent_prefetcher->handle_mem_access = &adjacent_handle_mem_access;
     adjacent_prefetcher->cleanup = &adjacent_cleanup;
 
-    // TODO allocate any additional memory needed to store metadata here and
+    // allocate any additional memory needed to store metadata here and
     // assign to adjacent_prefetcher->data.
 
     return adjacent_prefetcher;
@@ -85,22 +100,74 @@ struct prefetcher *adjacent_prefetcher_new()
 
 // Custom Prefetcher
 // ============================================================================
+struct address_list{
+    uint32_t address;
+    bool is_miss;
+    struct address_list *list;
+};
+
+struct custom_data{
+    int n;
+    struct address_list *visited;
+};
+
 uint32_t custom_handle_mem_access(struct prefetcher *prefetcher, struct cache_system *cache_system,
                                   uint32_t address, bool is_miss)
 {
-    // TODO perform the necessary prefetches for your custom strategy.
+    // perform the necessary prefetches for your custom strategy.
+    if(((struct custom_data*) prefetcher->data)->n > 0){
+        for(int i = 1; i <= ((struct custom_data*) prefetcher->data)->n; i++){
+        uint32_t fetch_address = address + i*cache_system->line_size;
+            cache_system_mem_access(cache_system, fetch_address, 'R', true);
+        }
+    }
+    int fetch_count = 0;
+    //the last address where a miss occurred while traversing access linked list
+    uint32_t last_missed = -1;
+    //the last address where a misss occurred after the last time the current address was loaded into memory
+    //is reset if a different address is the first one missed after the next time the current address was loaded into memeory
+    uint32_t already_missed = -1;
+    struct address_list* trav_list = ((struct custom_data*) prefetcher->data)->visited;
 
-    // TODO: Return the number of lines that were prefetched.
-    return 0;
+    while(fetch_count < 2 && trav_list->address != -1){
+        if(address == trav_list->address){
+            if(last_missed != -1 && last_missed == already_missed){
+                //printf("test\n");
+                fetch_count++;
+                cache_system_mem_access(cache_system, already_missed, 'R', true);
+            }
+            already_missed = last_missed;
+            last_missed = -1;
+        }else if(trav_list->is_miss){
+            last_missed = trav_list->address;
+        }
+        trav_list = trav_list->list;
+    }
+
+    struct address_list* new_address = malloc(sizeof(struct address_list));
+    new_address->address = address;
+    new_address->is_miss = is_miss;
+    new_address->list = ((struct custom_data*) prefetcher->data)->visited;
+    ((struct custom_data*) prefetcher->data)->visited = new_address;
+
+    // Return the number of lines that were prefetched.
+    return ((struct custom_data*) prefetcher->data)->n + fetch_count;
 }
 
 void custom_cleanup(struct prefetcher *prefetcher)
 {
-    // TODO cleanup any additional memory that you allocated in the
+    // cleanup any additional memory that you allocated in the
     // custom_prefetcher_new function.
+    while(((struct custom_data*) prefetcher->data)->visited->address != -1){
+        struct address_list *value = ((struct custom_data*) prefetcher->data)->visited;
+        ((struct custom_data*) prefetcher->data)->visited = ((struct custom_data*) prefetcher->data)->visited->list;
+        free(value);
+    }
+    free(((struct custom_data*) prefetcher->data)->visited);
+    free(((struct custom_data*) prefetcher->data));
 }
 
-struct prefetcher *custom_prefetcher_new()
+struct prefetcher *custom_prefetcher_new(uint32_t prefetch_amount)
 {
     struct prefetcher *custom_prefetcher = calloc(1, sizeof(struct prefetcher));
     custom_prefetcher->handle_mem_access = &custom_handle_mem_access;
@@ -108,6 +175,12 @@ struct prefetcher *custom_prefetcher_new()
 
     // TODO allocate any additional memory needed to store metadata here and
     // assign to custom_prefetcher->data.
+    struct custom_data *new_data = malloc(sizeof(struct custom_data));
+    new_data->n = prefetch_amount;
+    new_data->visited = (struct address_list*) malloc(sizeof(struct address_list));
+    new_data->visited->address = -1;
+    custom_prefetcher->data = new_data;
+    
 
     return custom_prefetcher;
 }
